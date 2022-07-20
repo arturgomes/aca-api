e649358019adbb504f1d9634166fb2e555ee51f4
Artur Oliveira Gomes <>
2022-07-20 10:12:24 -0400

    feat: :sparkles: routes for student controllers

---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import StudentController from "./controller/StudentController";
+import { Router } from 'express';
+
+/**
+ * I'm using this very basic set of routes for the api, nothing fancy for now
+ */
+
+const studentRoutes = Router();
+
+studentRoutes.get("/student", StudentController.all)
+studentRoutes.get("/student/:ra", StudentController.one)
+studentRoutes.post("/student/create", StudentController.create)
+studentRoutes.post("/student/update/:ra", StudentController.save)
+studentRoutes.delete("/student/:id", StudentController.remove)
+
+export default studentRoutes;
