2c8195bdd347fd97305c2339da0f56395ae2c2cd
Artur Oliveira Gomes <>
2022-07-20 10:12:55 -0400

    feat: :sparkles: connecting routes to app

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,20 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as morgan from 'morgan';
 import * as cors from 'cors';
+import studentRoutes from './routes'
 
 // in case of anything wrong, put the message forward
 function handleError(err, _req, res, _next) {
   res.status(err.statusCode || 500).send(err.message)
 }
 
-
+/**
+ * very tiny api using express. 
+ * 1 - I'm using morgan for logging the HTTP requests
+ * 2 - body-parser for using json as the content sent to the api
+ * 3 - studentRoutes contains the routes used in the api
+ * 4 - handleError for.. handling and sending errors ;=O
+ */
 const app = express();
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
@@ -27,6 +34,7 @@ const options: cors.CorsOptions = {
 };
 
 app.use(cors(options))
+app.use(studentRoutes);
 app.use(handleError);
 
 export default app;
